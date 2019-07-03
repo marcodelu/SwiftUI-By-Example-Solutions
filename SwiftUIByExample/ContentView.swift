1c42088aa1e71850ed36f98820a74bc5f2305152
Marco De Lucchi <marcodelucchi27@gmail.com>
2019-07-03 12:38:30 +0200

    How to create views in a loop using ForEach

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -8,14 +8,36 @@
 
 import SwiftUI
 
+struct Result {
+    var id = UUID()
+    var score: Int
+}
+
 struct ContentView : View {
+//    let colors: [Color] = [.red, .green, .blue]
+
+    let results = [Result(score: 8), Result(score: 5), Result(score: 10)]
 
     var body: some View {
-        Group {
-            if Bool.random() {
-                Image("example-image")
-            } else {
-                Text("Better luck next time")
+//        VStack(alignment: .leading) {
+//            ForEach((1...10).reversed()) {
+//                Text("\($0)…")
+//            }
+//
+//            Text("Ready or not, here I come!")
+//        }
+
+//        VStack {
+//            ForEach(colors.identified(by: \.self)) { color in
+//                Text(color.description.capitalized)
+//                    .padding(20)
+//                    .background(color)
+//            }
+//        }
+
+        VStack {
+            ForEach(results.identified(by: \.id)) { result in
+                Text("Result: \(result.score)")
             }
         }
     }
