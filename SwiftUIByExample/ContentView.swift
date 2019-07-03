6155a89f88d61c8d17cc678fd18664f6a5128e90
Marco De Lucchi <marco@Marco-di-MBP.localdomain>
2019-07-03 11:18:55 +0200

    Project init

---
+++ SwiftUIByExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIByExample
+//
+//  Created by Marco De Lucchi on 03/07/2019.
+//  Copyright © 2019 Marco De Lucchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
