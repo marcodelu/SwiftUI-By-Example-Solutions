ae26b20b32ee0f64bef5d493418645a3d1235d06
Marco De Lucchi <marcodelucchi27@gmail.com>
2019-07-04 10:40:20 +0200

    How to let users move rows in a list

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -17,14 +17,20 @@ struct ContentView : View {
                 ForEach(users.identified(by: \.self)) { user in
                     Text(user)
                 }
-                .onDelete(perform: delete)
+                .onMove(perform: move)
             }
+            .navigationBarItems(trailing: EditButton())
         }
     }
 
-    func delete(at offsets: IndexSet) {
-        if let first = offsets.first {
-            users.remove(at: first)
+    func move(from source: IndexSet, to destination: Int) {
+        // sort the indexes low to high
+        let reversedSource = source.sorted()
+
+        // then loop from the back to avoid reordering problems
+        for index in reversedSource.reversed() {
+            // for each item, remove it and insert it at the destination
+            users.insert(users.remove(at: index), at: destination)
         }
     }
 }
