3bd4744eb173d119fbe5d63def62e61559b0241b
Marco De Lucchi <marcodelucchi27@gmail.com>
2019-07-04 12:01:53 +0200

    How to push a new view onto a NavigationView

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -9,26 +9,18 @@
 import SwiftUI
 
 struct ContentView : View {
-    @State var showingSheet = false
-
-    var sheet: ActionSheet {
-        ActionSheet(title: Text("Action"),
-                    message: Text("Quote mark"),
-                    buttons: [
-                        .default(Text("Show Sheet"), onTrigger: {
-                            self.showingSheet = false
-                        })
-                    ]
-        )
-    }
-
     var body: some View {
-        Button(action: {
-            self.showingSheet = true
-        }) {
-            Text("Woo")
+        NavigationView {
+            NavigationLink(destination: DetailView()) {
+                Text("Click")
+            }.navigationBarTitle(Text("Navigation"))
         }
-            .presentation(showingSheet ? sheet : nil)
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
     }
 }
 
