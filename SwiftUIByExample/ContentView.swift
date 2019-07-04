2ad6ed6d0f7e392a98c1b6b46466992ad14dc5a5
Marco De Lucchi <marcodelucchi27@gmail.com>
2019-07-04 12:04:05 +0200

    How to push a new view when a list row is tapped

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -8,13 +8,40 @@
 
 import SwiftUI
 
-struct ContentView : View {
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct RestaurantRow: View {
+    var restaurant: Restaurant
+
     var body: some View {
-        NavigationView {
-            NavigationLink(destination: DetailView()) {
-                Text("Click")
-            }.navigationBarTitle(Text("Navigation"))
+        Text(restaurant.name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        let first = Restaurant(name: "Joe's Original")
+        let restaurants = [first]
+
+        return NavigationView {
+            List(restaurants) { restaurant in
+                NavigationLink(destination: RestaurantView(restaurant: restaurant)) {
+                    RestaurantRow(restaurant: restaurant)
+                }
+            }.navigationBarTitle(Text("Select a restaurant"))
         }
+    }
+}
+
+struct RestaurantView: View {
+    var restaurant: Restaurant
+
+    var body: some View {
+        Text("Come and eat at \(restaurant.name)")
+            .font(.largeTitle)
     }
 }
 
